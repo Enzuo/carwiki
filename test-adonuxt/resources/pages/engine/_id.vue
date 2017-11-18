b65ee5d1757c31749543bc063cd2c7b8c28a701f
Enzuo <potc.zone+git@gmail.com>
2017-11-18 17:42:45 +0100

    Add nuxt page to display engine

---
+++ test-adonuxt/resources/pages/engine/_id.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        {{message}} {{$route.params.id}}
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['id','params'],   
+    validate : ({params}) => {
+        console.log('validate params', params)
+        return /^\d+$/.test(params.id) || !params.id 
+    },
+    data : () => {
+        return {
+            message : 'hello engine'
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
