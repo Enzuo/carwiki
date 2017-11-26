e03ff08ca7b09c68a03c819f012802e860595cc0
Enzuo <potc.zone+git@gmail.com>
2017-11-26 21:33:28 +0100

    Add nested route, to seperate search bar from result display

---
+++ adonuxt4/resources/pages/engine.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <search-select
+      @select="selectEngine"
+    >
+    </search-select>
+    <nuxt-child/>
+  </div>
+</template>
+
+<script>
+import SearchSelect from '~/components/SearchSelect'
+
+
+export default {
+  components : {
+    SearchSelect
+  },
+  methods : {
+    selectEngine (id) {
+      console.log('route re push ', id)
+      this.$router.push({ path: `/engine/${id}`})
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- adonuxt4/resources/pages/engine/_id.vue
+++ adonuxt4/resources/pages/engine/_id.vue
@@ -1,26 +1,17 @@
 <template>
   <div>
-    <search-select
-      @select="selectEngine"
-    >
-    </search-select>
     {{message}} {{$route.params.id}} {{ engine }}
   </div>
 </template>
 
 <script>
 import axios from '~/plugins/axios'
-import SearchSelect from '~/components/SearchSelect'
-
 
 export default {
   props: ['id','params'],
   validate : ({params}) => {
       console.log('validate params', params)
       return /^\d+$/.test(params.id) || !params.id
-  },
-  components : {
-    SearchSelect
   },
   async asyncData ({ params }) {
     let engine = null
@@ -35,12 +26,6 @@ export default {
       message : 'hello engine'
     }
   },
-  methods : {
-    selectEngine (id) {
-      console.log('route re push ', id)
-      this.$router.push({ path: `/engine/${id}`})
-    }
-  }
 }
 </script>
 
