ad184bdbfb667d298e6875b5ac148b4b03f956d2
Enzuo <potc.zone+git@gmail.com>
2017-11-29 22:29:37 +0100

    Add very simple car page

---
+++ adonuxt4/resources/pages/car.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <search-select
+    >
+    </search-select>
+    <nuxt-child/>
+  </div>
+</template>
+
+<script>
+import SearchSelect from '~/components/SearchSelect'
+
+
+export default {
+  components : {
+    SearchSelect
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ adonuxt4/resources/pages/car/_id.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    A car
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+
+</style>
