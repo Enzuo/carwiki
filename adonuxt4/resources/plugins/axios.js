f2358abb64ebbbc58c393f7865f5e70a456dca64
Enzuo <potc.zone+git@gmail.com>
2017-12-31 10:11:09 +0100

    Enable access from the local network

--- adonuxt4/resources/plugins/axios.js
+++ adonuxt4/resources/plugins/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: process.env.baseUrl || 'http://127.0.0.1:3333/api'
+  baseURL: process.env.baseUrl || 'http://192.168.1.72:3333/api'
   // baseURL: 'http://127.0.0.1:3333/api'
 })
