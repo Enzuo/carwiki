eece8ca5bd8d674f9137675de78fc31032c0ee64
Enzuo <potc.zone+git@gmail.com>
2018-01-14 13:32:35 +0100

    Add lockable column

--- adonuxt4/resources/components/CarViewComponents/CarGears.vue
+++ adonuxt4/resources/components/CarViewComponents/CarGears.vue
@@ -6,6 +6,22 @@
       hide-actions
       item-key="name"
     >
+      <template slot="headers" scope="props">
+        <tr>
+          <th v-for="header in props.headers" :key="header.text"
+              :class="['column',
+                header.lockable ? 'sortable' : '',
+                header.active ? 'active' : '',
+                header.align === 'left' ? 'text-xs-left' : '',
+              ]"
+              @click="lockColumn(header.value)"
+          >
+            <v-icon v-if="header.lockable && (!header.align || header.align === 'right')">lock</v-icon>
+            {{ header.text }}
+            <v-icon v-if="header.lockable && header.align === 'left'">lock</v-icon>
+          </th>
+        </tr>
+      </template>
       <template slot="items" scope="props">
         <td><input :value="props.item.gear"></td>
         <td class="text-xs-right"><input :value="props.item.rpm" @input="changeRPM($event.target.value, props.index)"></td>
@@ -19,6 +35,7 @@
         </td>
       </template>
     </v-data-table>
+    {{ isRatioLocked }}
   </div>
 </template>
 
@@ -36,8 +53,8 @@ export default {
       headers : [
         {text: 'Gear', align: 'left', sortable: false, value: 'name'},
         {text: 'Rpm', align: 'left', sortable: false, value: 'rpm'},
-        {text: 'Speed (km/h)', align: 'left', sortable: false, value: 'speed'},
-        {text: 'Ratio', align: 'left', sortable: false, value: 'ratio'},
+        {text: 'Speed (km/h)', align: 'left', lockable: true, value: 'speed'},
+        {text: 'Ratio', align: 'left', lockable: true, value: 'ratio'},
       ],
       rpms
     }
@@ -56,6 +73,13 @@ export default {
         datas.push(gearData)
       }
       return datas
+    },
+    isRatioLocked : function () {
+      return !!this.headers.find((col) => {
+        if(col.value === 'ratio' && col.active === true){
+          return true
+        }
+      })
     }
   },
   methods : {
@@ -66,6 +90,9 @@ export default {
       Vue.set(this.car.gearRatio, index, value)
     },
     changeSpeed : function (speed, index) {
+      if(parseFloat(speed) < 1){
+        speed = 1
+      }
       var gearRatio = this.car.gearRatio
       var rpm = this.rpms[index]
       var maxGearRatio = gearRatio[gearRatio.length-1]
@@ -87,6 +114,7 @@ export default {
           var previousGearRatio = gearRatio[gearRatio.length-2]
           var previousGearSpeed = (maxGearRatio / previousGearRatio * maxGearSpeed) * (previousGearRPM / 1000)
           var newRatio = ((rpm/1000)*maxGearSpeed/(speed / maxGearRatio)) //* (rpm / 1000)
+          Vue.set(this.car.gearRatio, index, newRatio)
         }
 
         this.car.gearSpeed = speed / (rpm/1000)
@@ -102,7 +130,33 @@ export default {
     deleteRow : function (index) {
       console.log('delete ', index)
       this.car.gearRatio.splice(index, 1)
+    },
+    lockColumn : function (colValue) {
+      // Unlock previous different locked column
+      var indexAlreadyLocked = this.headers.findIndex(function (col) {
+        if(col.active === true && col.value !== colValue){
+          return true
+        }
+        return false
+      })
+      if(indexAlreadyLocked > -1){
+        var header = this.headers[indexAlreadyLocked]
+        header.active = false
+        Vue.set(this.headers, index, header)
+      }
+
+      // Lock or unlock clicked column
+      var index = this.headers.findIndex(function(col) {
+        if(col.value === colValue){
+          return true
+        }
+        return false
+      })
+      var header = this.headers[index]
+      header.active = header.active ? false : true
+      Vue.set(this.headers, index, header)
     }
+
   }
 }
 </script>
