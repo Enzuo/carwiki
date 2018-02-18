a8f495a93494eb59236a1acb26e3e9186b059295
Enzuo <potc.zone+git@gmail.com>
2018-02-18 19:13:10 +0100

    Add button to change language, we can see translations are generated server side first, then regenerated on the client...

--- adonuxt4/resources/pages/index.vue
+++ adonuxt4/resources/pages/index.vue
@@ -1,8 +1,10 @@
 <template>
   <section class="container">
     <img src="/img/logo.png" alt="Adonuxt logo"/>
-    <h1>Universal Vue.js Applications Framework</h1>
-    <span>With translation {{ $t('key') }}</span>
+    <h1>Universal Vue.js Applications Framework</h1><br>
+    <span>With translation {{ $t('key') }}</span><br>
+    <button @click="$i18n.i18next.changeLanguage('en')">Change to EN</button><br>
+    <button @click="$i18n.i18next.changeLanguage('cn')">Change to CN</button><br>
     <nuxt-link :to="{ name: 'about' }">About</nuxt-link>
   </section>
 </template>
--- adonuxt4/resources/plugins/vue-i18n.js
+++ adonuxt4/resources/plugins/vue-i18n.js
@@ -1,19 +1,40 @@
 import Vue from 'vue';
 import i18next from 'i18next';
 import VueI18next from '@panter/vue-i18next';
+// import Backend from 'i18next-node-fs-backend'
 
 Vue.use(VueI18next);
 
-i18next.init({
+var isClient = process.browser ? true : false
+console.log('VUE I18next init, client :', isClient)
+
+i18next
+// .use(Backend)
+.init({
   lng: 'en',
   fallbackLng: 'en',
   debug : true,
   resources : {
     en: {
       translation: {
-        "key": "hello world"
+        "key": "hello world "+isClient
+      }
+    },
+    cn: {
+      translation: {
+        "key": "chinese "
       }
     }
+  },
+  backend : {
+    // path where resources get loaded from
+    loadPath: '/locales/{{lng}}/{{ns}}.json',
+
+    // path to post missing resources
+    addPath: '/locales/{{lng}}/{{ns}}.missing.json',
+
+    // jsonIndent to use when storing json files
+    jsonIndent: 2
   }
 })
 
