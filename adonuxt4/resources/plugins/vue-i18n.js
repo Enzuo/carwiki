faba1d06b4a968852f272125c05d0dad585d3185
Enzuo <potc.zone+git@gmail.com>
2018-02-19 00:33:35 +0100

    Add language detection (but only working on browser)

--- adonuxt4/resources/plugins/vue-i18n.js
+++ adonuxt4/resources/plugins/vue-i18n.js
@@ -3,27 +3,56 @@ import i18next from 'i18next';
 import VueI18next from '@panter/vue-i18next';
 // import FetchBackend from 'i18next-fetch-backend';
 import XHRBackend from 'i18next-xhr-backend';
-
+import LngDetector from 'i18next-browser-languagedetector';
 
 Vue.use(VueI18next);
 
 var isClient = process.browser ? true : false
 console.log('VUE I18next init, client :', isClient)
 
+var detectorOpts = {
+  // order and from where user language should be detected
+  order: ['querystring', 'cookie', 'localStorage', 'navigator', 'htmlTag'],
+
+  // keys or params to lookup language from
+  lookupQuerystring: 'lng',
+  lookupCookie: 'i18next',
+  lookupLocalStorage: 'i18nextLng',
+
+  // cache user language on
+  caches: ['localStorage', 'cookie'],
+  excludeCacheFor: ['cimode'], // languages to not persist (cookie, localStorage)
+
+  // optional expire and domain for set cookie
+  cookieMinutes: 10,
+  // cookieDomain: 'carwiki',
+
+  // optional htmlTag with lang attribute, the default is:
+  htmlTag: isClient ? document.documentElement : false
+}
+
 if(isClient){
   i18next
   .use(XHRBackend)
+  .use(LngDetector)
 }
+
 
 // with this condition it'll be ignored on nuxt module build
 // https://github.com/nuxt/nuxt.js/issues/2458
 if (process.server) {
+  var servLngDetector = new LngDetector()
+
   const fsBackend = require('i18next-node-fs-backend')
-  i18next.use(fsBackend)
+  i18next.use(fsBackend).use(servLngDetector)
+
+  // Server side detection :
+  // https://github.com/i18next/react-i18next/issues/280
+
+  detectorOpts.order = ['servQueryString', 'servCookies']
 }
 
 const options = {
-  lng: 'en',
   fallbackLng: 'en',
   // keySeparator: false, // Allow usage of dots in keys
   debug : true,
@@ -34,14 +63,13 @@ const options = {
   backend : {
     // path where resources get loaded from
     loadPath: isClient ? '/locales/{{lng}}/{{ns}}.json' : 'resources/static/locales/{{lng}}/{{ns}}.json',
-  }
+  },
+  detection : detectorOpts,
 }
-
 
 if (!i18next.isInitialized) {
   i18next.init(options)
 }
-
 
 export default ({ app }, inject) => {
   // Set `i18n` instance on `app`
