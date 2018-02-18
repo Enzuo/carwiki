05a56c59363aa2cef0a0c5dc0beb71d96a7cc13b
Enzuo <potc.zone+git@gmail.com>
2018-02-18 17:14:26 +0100

    Clean translation implementation researches

--- adonuxt4/resources/plugins/vue-i18n.js
+++ adonuxt4/resources/plugins/vue-i18n.js
@@ -1,17 +1,11 @@
 import Vue from 'vue';
 import i18next from 'i18next';
-// import VueParams from 'vue-params';
 import VueI18next from '@panter/vue-i18next';
-// import VueI18Next from 'vue-i18next2';
 
-// Vue.use(VueParams);
 Vue.use(VueI18next);
 
-// Vue.params.i18nextLanguage = "en";
 i18next.init({
-// VueI18Next.i18n.init({
   lng: 'en',
-  // lng: Vue.params.i18nextLanguage,
   fallbackLng: 'en',
   debug : true,
   resources : {
@@ -22,8 +16,6 @@ i18next.init({
     }
   }
 })
-
-// const i18n = new VueI18next(i18next)
 
 export default ({ app }, inject) => {
   // Set `i18n` instance on `app`
